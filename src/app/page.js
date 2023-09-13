1ea26ea0929871b063b044e93c45d8be949aa197
Victor Caruso <vic.caruso@gmail.com>
2023-09-13 15:46:07 -0700

    Add metadata to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,11 @@ import BlogSummaryCard from '@/components/BlogSummaryCard';
 import styles from './homepage.module.css';
 import { getBlogPostList } from '@/helpers/file-helpers';
 
+export const metadata = {
+  title: 'Bits & Bytes',
+  description: 'A wonderful blog about JavaScript ',
+};
+
 async function Home() {
   const posts = await getBlogPostList();
   const postsWithId = posts.map((post) => ({

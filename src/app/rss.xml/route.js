ee179c53e246fc32822087125aa47d66374cfa51
Victor Caruso <vic.caruso@gmail.com>
2023-09-14 20:21:14 -0700

    Return XML of blog posts when visiting /rss.xml

--- src/app/rss.xml/route.js
+++ src/app/rss.xml/route.js
@@ -1,8 +1,28 @@
-import { NextResponse } from 'next/server';
+import { getBlogPostList } from '@/helpers/file-helpers';
+import RSS from 'rss';
 
 export async function GET() {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-  const data = await res.json();
+  const posts = await getBlogPostList();
+  const feed = new RSS({
+    title: "Vic Caruso's Blog",
+    description:
+      "Personal blog created using Next.js with the guidance of Josh Comeau's course - The Joy of React",
+    copyright: 'Vic Caruso 2023',
+    language: 'English',
+    pubDate: new Date(),
+  });
+  posts.forEach((post) => {
+    feed.item({
+      title: post.title,
+      description: post.abstract,
+      url: `${process.env.HOST}/${post.slug}`,
+      date: post.publishedOn,
+    });
+  });
+  const xml = feed.xml();
 
-  return NextResponse.json({ data });
+  return new Response(xml, {
+    status: 200,
+    headers: { 'Content-Type': 'application/xml' },
+  });
 }
